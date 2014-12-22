8254e18cfb9485ecff8ad889e16765dde3a5b369
delostik <delostik@126.com>
2014-12-22 13:56:43 +0800

    update gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = '121.41.62.10';
 $db['default']['username'] = 'DBSC';
-$db['default']['password'] = 'db2014';
+$db['default']['password'] = '';
 $db['default']['database'] = 'DBSC';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
