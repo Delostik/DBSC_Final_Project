b39999067eb6300d27216369c4614ad81e418486
delostik <delostik@126.com>
2014-12-23 08:01:46 +0800

    readd

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'nago08qGa9aaG4SHY';
+$config['encryption_key'] = 'nago08qsGa9aaG4SHY';
 
 /*
 |--------------------------------------------------------------------------
